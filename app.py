a58a8a23d8c8afefd69d694a42a1a159948459de
SiGae <sigae@protonmail.com>
2020-03-13 20:47:31 +0900

    test pagelink

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
+from flask import render_template
+from flask import jsonify
+import json
+from flask import request
+from flask import make_response
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
 	return render_template('index.html')
 
+@app.route('/login')
+def checklogin():
+	print("AS")
+	return render_template('index.html')
+
+
 if __name__ == '__main__':
 	app.run(host='0.0.0.0')
+
